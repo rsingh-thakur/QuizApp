4eaf47bd1ac51905a4996d8d754c78a1a894163d
Gourav Sahu <gouravsahu@newrise.in>
2023-10-09 15:12:41 +0530

    adds logger

--- src/main/java/com/nrt/quiz/QuizAppApplication.java
+++ src/main/java/com/nrt/quiz/QuizAppApplication.java
@@ -1,14 +1,20 @@
 package com.nrt.quiz;
 
+import org.slf4j.LoggerFactory;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import ch.qos.logback.classic.Logger;
 
 @SpringBootApplication
 public class QuizAppApplication {
 
 	public static void main(String[] args) {
+
+		Logger log = (Logger) LoggerFactory.getLogger(QuizAppApplication.class);
 		SpringApplication.run(QuizAppApplication.class, args);
-		System.out.println("Quiz App started Successfully ...");
+		log.info("Quiz app started successfully .......");
+
 	}
 
 }
