adc6334a15a554dc353aa23be842864c7c23ab61
rsingh-thakur <ramusingh72103@gmail.com>
2023-10-11 13:42:07 +0530

    adds encrypter and decrypter

--- src/main/java/com/nrt/quiz/serviceImpl/UserServiceImpl.java
+++ src/main/java/com/nrt/quiz/serviceImpl/UserServiceImpl.java
@@ -10,37 +10,39 @@ import com.nrt.quiz.entity.User;
 import com.nrt.quiz.repository.UserRepository;
 import com.nrt.quiz.request.UserRequest;
 import com.nrt.quiz.service.UserService;
+import com.nrt.quiz.util.CommonUtil;
 
 @Service
-public class userServiceImpl implements UserService {
+public class UserServiceImpl implements UserService {
 	@Autowired
 	UserRepository userRepository;
 
+  //create new user records
 	@SuppressWarnings("deprecation")
 	@Override
 	public User createUser(UserRequest userRequst, MultipartFile file) {
 
 		User user = new User();
-		user.setFirstName(userRequst.getFirstName());
-		user.setLastName(userRequst.getFirstName());
-		user.setImagePath(file.getOriginalFilename());
-		user.setEmailAddress(userRequst.getEmailAddress());
-		user.setPassword(userRequst.getPassword());
-		user.setUserType(userRequst.getUserType());
+
+		user.setFirstName(CommonUtil.encrypt(userRequst.getFirstName()));
+		user.setLastName(CommonUtil.encrypt(userRequst.getFirstName()));
+		user.setImagePath(CommonUtil.encrypt(file.getOriginalFilename()));
+		user.setEmailAddress(CommonUtil.encrypt(userRequst.getEmailAddress()));
+		user.setPassword(CommonUtil.encrypt(userRequst.getPassword()));
+		user.setUserType(CommonUtil.encrypt(userRequst.getUserType()));
 		user.setCreationDate(new Date(new java.util.Date().getDate()));
-        
+		//CommonUtil.saveFile(file);
 		return userRepository.save(user);
 	}
 
+  //checks user exits and password is correct
 	@Override
 	public Boolean login(String userId, String password) {
 
-		User user = userRepository.findByEmailAddress(userId);
-
-		if (user != null && user.getPassword().equalsIgnoreCase(password)) {
+		User user = userRepository.findByEmailAddress(CommonUtil.encrypt(userId));
+		if (user != null && CommonUtil.decrypt(user.getPassword()).equalsIgnoreCase(password)) {
 			return true;
 		}
-
 		return false;
 	}
 
